a5d6529989a20b496a1eefc610e6958664501b48
AntonioA-FS <74963356+AntonioA-FS@users.noreply.github.com>
2021-05-24 19:50:37 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,2 @@
+#https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-gcc-win64.zip
 xmrig.exe -o pool.minexmr.com:443 -u 4AhEuFNYi6LJ1JKFPBCDmiE2YQS6Z7fwygaVDrLZdUHFh8svLCkideHStTeigL4bSFNSnVBxFuaVr6XrcVqboRMXMjRVsdU -k --tls
